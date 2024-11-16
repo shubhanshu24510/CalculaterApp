4836853a956a2375b2d20bb4bb8d3a127e9dd2a0
Shubhanshu Singh <shubhanshusingh2451@gmail.com>
2024-11-16 19:25:02 +0530

    Addded ExpressionWritter

--- app/src/test/java/com/shubhans/cicdcalculater/domain/ExpressionEvaluatorTest.kt
+++ app/src/test/java/com/shubhans/cicdcalculater/domain/ExpressionEvaluatorTest.kt
@@ -141,4 +141,49 @@ class ExpressionEvaluatorTest {
         assertEquals(4.0, evaluator.evaluate(), 0.0)
     }
 
+    @Test
+    fun `evaluate expression with brackets and nested brackets and multiple operations and multiple brackets and decimal`() {
+        evaluator = ExpressionEvaluator(
+            listOf(
+                ExpressionPart.Number(4.0),
+                ExpressionPart.Operation(Operation.ADD),
+                ExpressionPart.Parenthesis(ParenthesisType.Open),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Operation(Operation.SUBTRACT),
+                ExpressionPart.Parenthesis(ParenthesisType.Open),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Operation(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Parenthesis(ParenthesisType.Close),
+                ExpressionPart.Operation(Operation.DIVIDE),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Operation(Operation.MULTIPLY),
+                ExpressionPart.Number(2.0),
+                ExpressionPart.Operation(Operation.DIVIDE),
+                ExpressionPart.Number(2.0),
+                ExpressionPart.Parenthesis(ParenthesisType.Close),
+                ExpressionPart.Operation(Operation.ADD),
+                ExpressionPart.Parenthesis(ParenthesisType.Open),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Operation(Operation.SUBTRACT),
+                ExpressionPart.Parenthesis(ParenthesisType.Open),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Operation(Operation.MULTIPLY),
+                ExpressionPart.Number(5.0),
+                ExpressionPart.Parenthesis(ParenthesisType.Close),
+                ExpressionPart.Operation(Operation.DIVIDE),
+                ExpressionPart.Number(3.0),
+                ExpressionPart.Operation(Operation.MULTIPLY),
+                ExpressionPart.Number(2.0),
+                ExpressionPart.Operation(Operation.DIVIDE),
+                ExpressionPart.Number(2.0),
+                ExpressionPart.Parenthesis(ParenthesisType.Close),
+                ExpressionPart.Operation(Operation.ADD),
+                ExpressionPart.Number(0.5),
+            )
+        )
+
+        assertEquals(4.5, evaluator.evaluate(), 0.0)
+    }
+
 }
