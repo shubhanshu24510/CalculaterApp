e096a730581c9a89e03c19969a3d5a9e50868834
Shubhanshu Singh <shubhanshusingh2451@gmail.com>
2024-11-19 13:39:54 +0530

    CalculaterUiThemeSetUp

---
+++ app/src/main/java/com/shubhans/cicdcalculater/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.shubhans.cicdcalculater.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
